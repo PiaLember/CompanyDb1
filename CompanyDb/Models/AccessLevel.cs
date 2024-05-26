b833c2d66b5ace67b7d74191d9a80929ac18f2a5
PiaLember <pia.lember@gmail.com>
2024-05-26 19:02:12 +0300

    Add AccessLevel.cs

---
+++ CompanyDb/Models/AccessLevel.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace CompanyDb.Models
+{
+    public class AccessLevel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AccessLevelId { get; set; }
+
+        [ForeignKey("Employee")]
+        public int EmployeeId { get; set; }
+
+        public required Employee Employee { get; set; }  
+
+        public int Level { get; set; }
+
+        [StringLength(30)]
+        public string Description { get; set; }
+
+        [StringLength(30)]
+        public string AccessType { get; set; }
+
+        [StringLength(100)]
+        public string? Comment { get; set; }
+    }
+}
--- CompanyDb/obj/Debug/net8.0/CompanyDb.AssemblyInfo.cs
+++ CompanyDb/obj/Debug/net8.0/CompanyDb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyDb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0961ac15a4b6ed1cb52e65845e1165290618dd50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b72e8f74f29e02e20efde27424475469180b3507")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyDb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyDb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
