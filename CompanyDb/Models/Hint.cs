903f4e0f25c62e7bf58e2fc6bf260859d389fee2
PiaLember <pia.lember@gmail.com>
2024-05-26 18:40:46 +0300

    Add Hint.cs

---
+++ CompanyDb/Models/Hint.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace CompanyDb.Models
+{
+    public class Hint
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int HintId { get; set; }
+
+        [StringLength(25)]
+        public required string Topic { get; set; }
+
+        [StringLength(1000)]
+        public required string Description { get; set; }
+
+        public bool Anonymous { get; set; }
+
+        [StringLength(100)]
+        public string? Comment { get; set; }
+    }
+}
--- CompanyDb/obj/Debug/net8.0/CompanyDb.AssemblyInfo.cs
+++ CompanyDb/obj/Debug/net8.0/CompanyDb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyDb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+373e8536553db85a52b00dad7311927751677c79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efc1cb257418c84af3ca3d731f1c56492bd8bfae")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyDb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyDb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
